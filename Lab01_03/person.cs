c49da50a6a3860e326612d2774b6390e668e939d
NghiaZxz <huunghia14665@gmail.com>
2023-09-07 09:58:24 +0700

    first commit

---
+++ Lab01_03/person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01_03
+{
+    class Class1
+    {
+    }
+}
---
+++ Lab01_03/student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01_03
+{
+    class student:person
+    {
+        private float averageScore;
+        private string faculty;
+
+        public student(string iD, string fullName,float averageScore, string faculty)
+        {
+            this.averageScore = averageScore;
+            this.faculty = faculty;
+        }
+
+        public float AverageScore { get => averageScore; set => averageScore = value; }
+        public string Faculty { get => faculty; set => faculty = value; }
+    }
+}
